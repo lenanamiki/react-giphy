d3cbcab6b7d9366a0ea38e31aac32602c62f56d3
Lena Namiki <lena.namiki@gmail.com>
2021-07-13 13:13:18 +0900

    Used giphy api to make gif search dynamic

--- workshop/src/components/app.jsx
+++ workshop/src/components/app.jsx
@@ -3,6 +3,12 @@ import SearchBar from "./searchBar";
 import Gif from "./gif";
 import GifList from "./gifList";
 
+// import giphy api
+const giphy = require('giphy-api')({
+  apiKey: 'KsltJNEs1v3QDDVlinP6EFo2GqjFxgRR',
+  https: true
+});
+
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   constructor() {
@@ -19,12 +25,24 @@ class App extends Component {
     });
   };
 
+  changeGifIds = (keyword) => {
+    giphy.search({
+      q: keyword,
+      rating: 'g',
+      limit: 10
+    }, (err, res) => {
+      this.setState({
+        ids: res.data.map(gif => gif.id)
+      });
+    });
+  };
+
   render() {
     const { selectedId, ids } = this.state;
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar changeGifIds={this.changeGifIds}/>
           <div className="selected-gif">
             <Gif id={selectedId} />
           </div>
--- workshop/src/components/searchBar.jsx
+++ workshop/src/components/searchBar.jsx
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class SearchBar extends Component {
+  handleChange = (e) => {
+    const { changeGifIds } = this.props;
+    const keyword = e.currentTarget.value;
+    changeGifIds(keyword);
+  };
+
   render() {
     return (
-      <input type="text" className="form-control form-search" placeholder="Search a GIF" />
+      <input type="text" className="form-control form-search" placeholder="Search a GIF" onChange={this.handleChange} />
     );
   }
 }
