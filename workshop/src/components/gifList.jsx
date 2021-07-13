58cd12e01b18452bfc49e71a327fb5743f0b7b15
Lena Namiki <lena.namiki@gmail.com>
2021-07-13 11:40:07 +0900

    Created GifList component

--- workshop/src/components/gifList.jsx
+++ workshop/src/components/gifList.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Gif from "./gif";
 
 // eslint-disable-next-line react/prefer-stateless-function
 class GifList extends Component {
   render() {
     return (
-      <div></div>
+      <div className="gif-list">
+        <Gif />
+        <Gif />
+        <Gif />
+        <Gif />
+      </div>
     );
   }
 }
